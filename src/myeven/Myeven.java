20aa5067a37a73aad054ca6789f9ec7af73e2949
PetraMonicah <patrahnikki56@gmail.com>
2024-10-04 18:19:44 +0300

    Add files via upload

---
+++ src/myeven/Myeven.java
@@ -0,0 +1,18 @@
+package myeven;
+import java.util.Scanner;
+
+public class Myeven {
+
+	public static void main(String[] args) {
+		System.out.println("Even from 1 to 20 are:");
+		for(int i =1;i <= 20;i++) {
+			if(i% 2 ==0) {
+				System.out.println(i);
+			}
+		}
+		
+		
+
+	}
+
+}
